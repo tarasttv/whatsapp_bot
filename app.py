afbc97f052b1d5bdf11ac84f717a1c23ccf3d07d
taras.ttv <taras.ttv@gmail.com>
2025-07-28 19:05:17 +0500

    Fix: listen on 0.0.0.0 and PORT for Render

--- app.py
+++ app.py
@@ -122,6 +122,8 @@ def save_to_sheet(phone, dialog):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
